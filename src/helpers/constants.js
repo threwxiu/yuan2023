0a82b3ebecee21ac14347f48a725c0cc429047c9
threwxiu <82743508+threwxiu@users.noreply.github.com>
2022-12-31 19:51:08 +0800

    Initial commit

---
+++ src/helpers/constants.js
@@ -0,0 +1,11 @@
+exports.ALL_NOTE_SETTINGS= [
+    "dgHomeLink",
+    "dgPassFrontmatter",
+    "dgShowBacklinks",
+    "dgShowLocalGraph",
+    "dgShowInlineTitle",
+    "dgShowFileTree",
+    "dgEnableSearch",
+    "dgShowToc",
+    "dgLinkPreview"
+];
